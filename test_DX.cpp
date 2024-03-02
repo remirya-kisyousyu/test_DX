7f51850ab20b3e358c34159c9995b4c65431c1fd
remirya-kisyousyu <155557223+remirya-kisyousyu@users.noreply.github.com>
2024-03-02 13:21:22 +0900

    Update test_DX.cpp

--- test_DX.cpp
+++ test_DX.cpp
@@ -1,4 +1,4 @@
-﻿// test_DX.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
+// test_DX.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
 //
 
 #include "DxLib.h"
@@ -49,15 +49,75 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _
 	cheeseCoordX = rand() % screenSizeX;
 	cheeseCoordY = rand() % screenSizeY;
 
-	//画像の表示
-	DrawGraph(mouseCoordX, mouseCoordY, mouse, FALSE);
-	DrawGraph(catCoordX, catCoordY, cat, FALSE);
-	DrawGraph(cheeseCoordX, cheeseCoordY, cheese, FALSE);
+	//文字列の表示
+	int charaColor;
+	charaColor = GetColor(255, 255, 255); //白
 
-	//flip
+	DrawFormatString(screenSizeX / 2 - 60, screenSizeY / 2, charaColor,  "PRESS ANY BUTTOM");
 	ScreenFlip();
 
+	//ゲームスタート待機
 	WaitKey();
+
+	int currPoint = 0;
+
+	while (TRUE) {
+		//スクリーンクリア
+		ClearDrawScreen();
+
+		//移動処理
+		if (CheckHitKey(KEY_INPUT_LEFT) == 1) {
+			//左移動
+			mouseCoordX -= stepSize;
+		}
+		if (CheckHitKey(KEY_INPUT_RIGHT) == 1) {
+			//右移動
+			mouseCoordX += stepSize;
+		}
+		if(CheckHitKey(KEY_INPUT_UP) == 1) {
+			//上移動
+			mouseCoordY -= stepSize;
+		}
+		if (CheckHitKey(KEY_INPUT_DOWN) == 1) {
+			//下移動
+			mouseCoordY += stepSize;
+		}
+		
+		//スクリーン外に出ないようチェック
+		if (mouseCoordX < 0) {
+			mouseCoordX = 0;
+		}
+		else if (mouseCoordX > screenSizeX - sizeMouseX) {
+			mouseCoordX = screenSizeX - sizeMouseX;
+		}
+
+		if (mouseCoordY < 0) {
+			mouseCoordY = 0;
+		}
+		else if (mouseCoordY > screenSizeY - sizeMouseY) {
+			mouseCoordY = screenSizeY - sizeMouseY;
+		}
+		
+		//文字列の表示
+		DrawFormatString(0, screenSizeY - 60, charaColor, "得点：%d", currPoint);
+
+		//画像の表示
+		DrawGraph(mouseCoordX, mouseCoordY, mouse, FALSE);
+		DrawGraph(catCoordX, catCoordY, cat, FALSE);
+		DrawGraph(cheeseCoordX, cheeseCoordY, cheese, FALSE);
+
+		//スクリーンをコピー
+		ScreenFlip();
+
+		//速度制限
+		WaitTimer(20);
+
+		//windowsメッセージ処理
+		if (ProcessMessage() == -1) break;
+
+		//ESCキーで終了
+		if (CheckHitKey(KEY_INPUT_ESCAPE) == 1) break;
+	}
 
 	DxLib_End();
 
